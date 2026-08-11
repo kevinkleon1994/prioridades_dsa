3a4faec80ee9d40003fd47aa3e6cc7bcea26c8b3
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-11 02:08:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyOYNt-2hXMv6Wcq1DlhCkJEIuTuIl0agBA9wKH2tuk3A8BDo12cqS_T8Ob_wmAL2H1MQ/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbycgioB8RoCxa3CZF9_eZilZ6B16xsokSBD7SAruwPD4vX-WXtnlw3TPNw7urRdm4qa/exec",
   DISTRICT: "Castelo de Sonhos",
   FIELD: "Missão Oeste do Pará",
   UNION: "União Norte Brasileira",
